e0efb150a15c498cf36e1017261f34e0c9351b8f
Jairus <117532457+jai-rus@users.noreply.github.com>
2025-04-21 00:08:27 -0700

    Initial Code
    Set up some macros and began printing menu. Started on printing initial state of board

---
+++ CS2640FinalProjectGroup9.asm
@@ -0,0 +1,72 @@
+#An Nguyen, Jairus Legion, Valeria Urzua, Viet Tran
+#CS2640
+#Final Project Group 9
+.include "CS2640FinalProjectGroup9Macros.asm"
+
+#Board Size is 3x3
+.eqv BOARD_SIZE 9
+
+.data
+board: .space BOARD_SIZE
+welcomeMsg: .asciiz "Welcome to our Tic-Tac-Toe game! To get started, please see the options below!"
+mainMenu: .asciiz "\n---------\nTo play a game please enter (1)\nTo exit please enter (2)\n---------\n"
+echo: .asciiz "You chose: "
+invalidInput: .asciiz "Invalid choice. Please try again\n"
+startMenu: .asciiz "\nNow starting a game.\n"
+exitMsg: .asciiz "\nThanks for playing! Goodbye!"
+newLine: .asciiz "\n"
+verticalLine: .asciiz " | "
+horizontalLine: .asciiz "-----------"
+
+#Maybe save $t0 as the main input variable
+
+.text
+#Print Welcome Message then jump to main menu
+main:
+	printString(welcomeMsg)
+	j menu
+
+#Prints menu then waits for input	
+menu:
+	printString(mainMenu)
+	#Takes in input then echos back to user
+	getInt()
+	printString(echo)
+	printInt($t0)
+	
+	#If input equals startGame
+	li $t1, 1
+	beq $t0, $t1, startGame
+	
+	#If input equals exit
+	li $t1, 2
+	beq $t0, $t1, exit
+	
+	#Invalid Input
+	printString(invalidInput)
+	j menu
+	
+startGame:
+	printString(startMenu)
+	#Index of the position on the board
+	li $t1, 0
+	j printInitialBoard
+	
+printInitialBoard:
+	printInt($t1)
+	
+	li $t3, 2
+	beq $t1, $t3, printHorizontal
+	add $t1, $t1, 1
+	printString(verticalLine)
+	j printInitialBoard
+	
+printHorizontal:
+	printString(newLine)
+	printString(horizontalLine)
+	
+exit:
+	printString(exitMsg)
+	li $v0, 10
+	syscall
+	
---
+++ CS2640FinalProjectGroup9Macros.asm
@@ -0,0 +1,19 @@
+#Prints out a string
+.macro printString(%msg)
+	li $v0, 4
+	la $a0, %msg
+	syscall
+.end_macro
+
+#Takes in an integer input and save its in $t0
+.macro getInt()
+	li $v0, 5
+	syscall
+	move $t0, $v0
+.end_macro
+
+.macro printInt(%address)
+	li $v0, 1
+	move $a0, %address
+	syscall
+.end_macro
